cf519f78eff803fc22e48112f81f385e17faac85
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2024-03-12 15:36:17 +0000

    :sparkles: feat: exercise

---
+++ ex.pl
@@ -0,0 +1,35 @@
+% 	homem(nome)
+homem("stannis").
+homem("renly").
+homem("robert").
+homem("tywin").
+homem("kevan").
+homem("ser jaime").
+homem("tyrion").
+homem("joffrey").
+homem("tommen").
+homem("lancel").
+homem("loras tyrell").
+homem("gandor clegane").
+homem("ser gregor clegane").
+homem("aerys ii").
+homem("rhaegar").
+homem("viserys").
+homem("khal drogo").
+homem("rhaenys").
+homem("aegon").
+
+
+
+% 	mulher(nome)
+mulher("joanna").
+mulher("dorna").
+mulher("cersei").
+mulher("myrcella").
+mulher("rhaella").
+mulher("elia").
+mulher("daenerys").
+
+
+
+% 	familia(nome, nomeFamilia)
